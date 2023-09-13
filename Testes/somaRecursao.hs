a01dd61f10af53f4916ca3710ec03e61b6d4ae7b
Sergio Henrique <114961377+shenrique08@users.noreply.github.com>
2023-09-13 16:23:44 -0300

    new exs

---
+++ Testes/somaRecursao.hs
@@ -0,0 +1,11 @@
+somaRecursao :: Int -> Int
+somaRecursao n
+    | n == 0 = 0
+    | otherwise = n + somaRecursao(n - 1)
+
+
+
+fatorial :: Int -> Int
+fatorial n
+     | n == 0 || n == 1 = 1
+     | otherwise = n * fatorial(n - 1)
